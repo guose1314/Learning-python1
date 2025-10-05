1d63f22c86d5601af4543fea284264826de193ab
guose1314 <47658712+guose1314@users.noreply.github.com>
2025-10-05 22:47:09 +0800

    Add files via upload

---
+++ IP ping网络测试/ping测试/ping测试/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using System.Net;
+using System.Net.NetworkInformation;
+namespace ping测试
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void huiche() //判断是否按下回车
+        {
+            button1_Click(null, null);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            textBox2.Enabled = false;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox2.Enabled != true)
+            {
+
+                if (textBox1.Text != "")
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    checkBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    timer1.Start();
+                }
+                else
+                {
+                    MessageBox.Show("请输入IP-如192.168.0.1", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+            else
+            {
+                if (textBox1.Text != "" && textBox2.Text != "")
+                {
+                    button1.Enabled = false;
+                    button2.Enabled = true;
+                    checkBox1.Enabled = false;
+                    textBox2.Enabled = false;
+                    timer2.Start();
+                }
+                else
+                {
+                    MessageBox.Show("请输入IP-如(192.168.0.1)\n请输入地址位数-如(1-254)", "提醒", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            checkBox1.Enabled = true;
+            textBox2.Enabled = true;
+            timer1.Stop();
+            timer2.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+           
+
+
+                    bool online = false; //是否在线
+                    Ping ping = new Ping();
+                    string ipstr = textBox1.Text; //获取IP
+                    string ipaddss = textBox2.Text;
+                    int timeout = 100; //设置超时显示时间
+                    //准备测试数据
+                    string data = "test data";
+                    byte[] buffer = Encoding.ASCII.GetBytes(data);
+
+                    //数据发送开始
+                    PingReply pingReply = ping.Send(ipstr, timeout, buffer);
+                    if (pingReply.Status == IPStatus.Success)
+                    {
+                        online = true;
+                        //MessageBox.Show("当前在线，已ping通！");
+
+                    }
+                    string txt = "";
+                    if (online)
+                    {
+                        txt = "当前在线，已ping通！" + "   IP—" + ipstr + "   时间--" + DateTime.Now.ToString();
+                        listBox1.Items.Insert(0, txt);
+                    }
+                    else
+                    {
+                        txt = "不在线，ping不通！" + "   IP—" + ipstr + "    时间--" + DateTime.Now.ToString();
+                        listBox2.Items.Insert(0, txt);
+                    }
+                }
+                
+                
+                      
+        
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox2.Items.Clear();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)//判断是否按下回车
+            {
+                huiche();
+            }
+            //如果输入的不是数字键，也不是回车键、Backspace键，则取消该输入
+            //if (!(Char.IsNumber(e.KeyChar)) && e.KeyChar != (char)13 && e.KeyChar != (char)8)
+            //{
+            //    e.Handled = true;
+            //}
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)//判断是否按下回车
+            {
+                huiche();
+            }
+            //如果输入的不是数字键，也不是回车键、Backspace键，则取消该输入
+            //if (!(Char.IsNumber(e.KeyChar)) && e.KeyChar != (char)13 && e.KeyChar != (char)8)
+            //{
+            //    e.Handled = true;
+            //}
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) //检测是否勾选
+            {
+                textBox2.Enabled = true;
+            }
+            else
+            {
+                textBox2.Enabled = false;
+                textBox2.Text = "";
+            }
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+               
+
+                  
+                    Ping ping = new Ping();
+                    string ipstr = textBox1.Text; //获取IP
+                    string ipaddss = textBox2.Text; //获取地址位数
+                    string[] FENKAI = ipstr.Split('.');
+                    int fenkai = Convert.ToInt32(FENKAI[3]);//将STRING类型的转成INT型取第3数组里面的内容
+                    int dizhi = Convert.ToInt32(ipaddss);
+                    int timeout = 100; //设置超时显示时间
+                    //准备测试数据
+                    string data = "test data";
+                    byte[] buffer = Encoding.ASCII.GetBytes(data);
+                    int[] name;
+                    name = new int[dizhi];
+
+                    for (int ii = fenkai; ii <= dizhi; ii++)
+                    {
+                       
+                        for (int i = 0; i <= dizhi; i++)
+                        {
+                            name[i] = ii;
+                            break;
+
+                        }
+                        bool online1 = false;
+                        //数据发送开始
+                        string ip = ipstr.Substring(0, ipstr.LastIndexOf(".") + 1);
+                        string ipp = ip + name[0];
+                        PingReply pingReply = ping.Send(ipp, timeout, buffer);
+                        if (pingReply.Status == IPStatus.Success)
+                        {
+                            online1 = true;
+                            //MessageBox.Show("当前在线，已ping通！");
+
+                        }
+                        string txt = "";
+                        if (online1)
+                        {
+                            txt = "当前在线，已ping通！" + "   IP—" + ipp + "   时间--" + DateTime.Now.ToString();
+                            listBox1.Items.Insert(0, txt);
+                           
+                        }
+                        else
+                        {
+                            txt = "不在线，ping不通！" + "   IP—" + ipp + "    时间--" + DateTime.Now.ToString();
+                            listBox2.Items.Insert(0, txt);
+                        }
+                      
+
+                    }
+                
+            }
+          
+        }
+
+
+    }
+
+        
+
+   
+    
+     
+        
+    
+
---
+++ IP ping网络测试/ping测试/ping测试/Form2.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ping测试
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+   
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            // 获取系统内所有进程  
+            System.Diagnostics.Process[] currentprocess = System.Diagnostics.Process.GetProcesses();
+            foreach (System.Diagnostics.Process p in currentprocess)
+            {
+                listBox1.Items.Add(p.ToString());
+               // System.Console.WriteLine(p.ToString());
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //结束进程
+            if (listBox1.Items.Count > 0)
+            {
+                string a = listBox1.SelectedItem.ToString();
+                int indexS = a.IndexOf("(");//获取到从0到“（”的长度
+                int indexE = a.IndexOf(")");//获取到从0到“）”的长度
+                if (indexS >= 0)
+                {
+                    string b = a.Substring(indexS + 1, indexE - indexS - 1);
+                    textBox2.Text = b;
+                    System.Diagnostics.Process[] killprocess = System.Diagnostics.Process.GetProcessesByName(b);
+                    foreach (System.Diagnostics.Process p in killprocess)
+                    {
+                        p.Kill();
+                        listBox1.Items.Remove(listBox1.SelectedItem);
+                        textBox1.Text = "进程已删除";
+                    }
+                }
+            }
+        }
+        
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+                //首先判断列表框中的项是否大于0
+            if (listBox1.Items.Count > 0) 
+                 {
+                 //清空所有项
+                     listBox1.Items.Clear();
+                 }
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = listBox1.SelectedItem.ToString();
+        }
+    }
+}
+    
+
